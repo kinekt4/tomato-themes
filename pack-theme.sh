e4c88ce461093c04457b499cdab9bea7f3ecf10e
Vuong V <3781991+kinekt4@users.noreply.github.com>
2020-10-14 09:45:14 +1100

    Packing script operational

--- pack-theme.sh
+++ pack-theme.sh
@@ -24,23 +24,25 @@ if [ ! -d "${theme_dir_path}" ]; then
     exit 1
 fi
 
-theme_file="${theme_dir_path}/theme.css"
-new_theme_file="${theme_dir_path}/${theme_name}.css"
+theme_file="theme.css"
+# theme_file_path="${theme_dir_path}/${theme_file}"
+new_theme_file="${theme_name}.css"
+# new_theme_file_path="${theme_dir_path}/${new_theme_file}"
 zip_file="${theme_name}.zip"
-zip_file_path="${zip_base_path}/${zip_filetheme_name}"
+zip_file_path="${zip_base_path}/${zip_file}"
 
 if [ -f "${zip_file_path}" ]; then
 
     echo "Caution: Zip file exists"
+    echo "Overwriting..."
 
 fi
 
 echo "Creating zip file: ${zip_file_path}"
+cd "${theme_dir_path}"
 zip "${zip_file_path}" "${theme_file}"
 
-cd "${zip_base_path}"
-echo "Renaming theme.css to ${theme_name}.css"
-printf "@ theme.css\n@=$theme_name.css\n"
-printf "@ theme.css\n@=$theme_name.css\n" | zipnote -w "${zip_file}"
+echo "Renaming ${theme_file} to ${new_theme_file}"
+printf "@ ${theme_file}\n@=${new_theme_file}\n" | zipnote -w "${zip_file_path}"
 
 echo "Done."
