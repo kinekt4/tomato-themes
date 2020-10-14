4ea7801a0accfb863e019bf06b992993b9d985b1
Vuong V <3781991+kinekt4@users.noreply.github.com>
2020-10-14 12:16:12 +1100

    Added clone from zip script. renamed clone-theme to clone-from-theme

---
+++ clone-from-zip.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+curr_dir=$(pwd)
+theme_name=$1
+new_theme_name=$2
+
+orig_theme_file="theme.css"
+theme_file="${theme_name}.css"
+theme_base_path="${curr_dir}/themes"
+zip_base_path="${curr_dir}/zip"
+theme_dir_path="${theme_base_path}/${theme_name}"
+new_theme_dir_path="${theme_base_path}/${new_theme_name}"
+new_theme_file_path="${new_theme_dir_path}/${orig_theme_file}"
+zip_file="${theme_name}.zip"
+zip_file_path="${zip_base_path}/${zip_file}"
+new_zip_file="${new_theme_name}.zip"
+new_zip_file_path="${zip_base_path}/${new_zip_file}"
+
+show_help() {
+    echo "Usage:"
+    echo "./clone-from-zip.sh <theme-name> <new-theme-name>"
+}
+
+if [ -z "${theme_name}" ]; then
+
+    show_help
+    exit 1
+fi
+
+if [ -z "${new_theme_name}" ]; then
+
+    show_help
+    exit 1
+fi
+
+if [ ! -f "${zip_file_path}" ]; then
+
+    echo "Theme zip file not found: ${zip_file_path}"
+    exit 1
+fi
+
+if [ -f "${new_zip_file_path}" ]; then
+
+    echo "Caution: Theme zip file exists"
+    echo "Overwriting..."
+fi
+
+echo "Copying: ${zip_file_path}"
+echo "To: ${new_zip_file_path}"
+
+cp "${zip_file_path}" "${new_zip_file_path}"
+
+if [ ! -d "${new_theme_dir_path}" ]; then
+
+    mkdir -p "${new_theme_dir_path}"
+fi
+
+echo "Renaming ${theme_file} to ${orig_theme_file}"
+printf "@ ${theme_file}\n@=${orig_theme_file}\n" | zipnote -w "${new_zip_file_path}"
+
+unzip "${new_zip_file_path}" -d "${new_theme_dir_path}"
+
+echo "Done."
